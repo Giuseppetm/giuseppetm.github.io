edacf33ab1a4806ed0b462bbf34f3d8af980f3d4
Giuseppetm <gdeambo@yahoo.it>
2021-03-04 21:51:17 +0100

    Added particles

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,39 @@ import About from './About';
 import Projects from './Projects';
 import Other from './Other';
 import Footer from './Footer';
+import ParticleComponent from "./ParticleComponent";
 
 const App = () => {
   return (
     <div>
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          zIndex: 50
+        }}
+      >
+        <ParticleComponent />
+      </div>
+      <div
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            zIndex: 0
+          }}
+        >
       <Home />
       <About />
       <Projects />
       <Other />
       <Footer />
+      </div>
     </div>
   );
 }
